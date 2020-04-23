e1523cffb6ba4729f894804556dcbc3277955ba8
Nans Pellicari <nans.pellicari@gmail.com>
2020-04-23 12:49:26 +0200

    Add license

--- Source/UE4HowtoCustomPin/UE4HowtoCustomPin.cpp
+++ Source/UE4HowtoCustomPin/UE4HowtoCustomPin.cpp
@@ -1,3 +1,5 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
 #include "UE4HowtoCustomPin.h"
 
 #include "Modules/ModuleManager.h"
--- Source/UE4HowtoCustomPin/UE4HowtoCustomPinGameModeBase.cpp
+++ Source/UE4HowtoCustomPin/UE4HowtoCustomPinGameModeBase.cpp
@@ -1,1 +1,3 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
 #include "UE4HowtoCustomPinGameModeBase.h"
